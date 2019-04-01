c7e1af7d459e3b7aaa5ba494ceb33c766a2c124e
Mika Rinne <mika.rinne@oracle.com>
2019-04-01 14:45:36 +0300

    database creation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var request = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -64,4 +65,67 @@ app.get('/testconnection', function(req, res) {
             });
         }
     });
+});
+
+var doconnect = function(cb) {
+    oracledb.getConnection({
+        user: dbConfig.dbuser,
+        password: dbConfig.dbpassword, connectString: dbConfig.connectString
+        },
+    cb
+)};
+
+var dorelease = function(conn) {
+  conn.close(function (err) {
+    if (err)
+      console.error(err.message);
+    });
+};
+
+var dodrop = function (conn, cb) {
+    conn.execute(
+    `BEGIN 
+        EXECUTE IMMEDIATE 'DROP TABLE j_purchaseorder'; EXCEPTION WHEN OTHERS THEN
+        IF SQLCODE <> -942 THEN
+                 RAISE;
+               END IF;
+    END;`,
+    function(err) {
+      if (err) {
+        return cb(err, conn);
+      } else {
+        console.log("Table dropped"); return cb(null, conn);
+      }
+    });
+};
+
+var docreate = function (conn, cb) {
+    conn.execute(
+    `CREATE TABLE j_purchaseorder (po_document VARCHAR2(4000) CONSTRAINT ensure_json CHECK (po_document IS JSON))`,
+    function(err) {
+      if (err) {
+        return cb(err, conn);
+      } else {
+        console.log("Table created");
+        return cb(null, conn);
+      }
+    });
+};
+
+app.get('/createdb', function(req, res) {
+  async.waterfall(
+  [
+    doconnect,
+    dodrop,
+    docreate
+  ],
+  function (err, conn) {
+    if (err) { 
+        console.error("In waterfall error cb: ==>", err, "<==");
+    }
+    if (conn)
+    {
+        dorelease(conn);
+    }
+  });
 });
