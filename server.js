66ff9f21d665f228f6aab9bb326df0eba846014d
Mika Rinne <mika.rinne@oracle.com>
2019-04-02 10:54:42 +0300

    data read

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/data', function(req, res) {
                                 res.send(JSON.stringify(response));
                             } else {
                                 var response = {};
-                                var response.data = [];
+                                response.data = [];
                                 for(var i=0; i < result.length; i++)
                                 {
                                     console.log(result.rows[i].[1]);
