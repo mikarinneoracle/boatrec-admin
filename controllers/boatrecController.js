94b4d6162121e15e9aaf643d07c686cbaf132c7e
Mika Rinne <mika.rinne@oracle.com>
2019-04-10 10:20:06 +0300

    get data modified

--- controllers/boatrecController.js
+++ controllers/boatrecController.js
@@ -8,10 +8,10 @@ app.controller('boatrecController', function($location, $http, $rootScope, $scop
         $http.get('/data').success(function(response, err) {
             for(var i = 0; i < response['data'].length; i++) {
                 var row = {};
-                row.key1 = response['data'][i].key1;
-                row.key2 = response['data'][i].key2;
-                row.key3 = response['data'][i].key3;
-                row.key4 = response['data'][i].key4;
+                row.key1 = response[i].key1;
+                row.key2 = response[i].key2;
+                row.key3 = response[i].key3;
+                row.key4 = response[i].key4;
                 data.push(row);
             }
             $scope.boatrecData = data;
