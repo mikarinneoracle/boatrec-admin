6bf46ac1e0a279787eab01b71b4df0473e183931
Mika Rinne <mika.rinne@oracle.com>
2019-04-10 14:01:15 +0300

    testing

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/posts', function(req, res) {
         if(!error && response)
         {
             res.setHeader("Content-Type", "application/json");
-            res.setHeader('Content-Encoding', 'br');
+            res.setHeader("Content-Encoding", "br");
             var data = JSON.parse(body);
             res.send(data);
         } else {
