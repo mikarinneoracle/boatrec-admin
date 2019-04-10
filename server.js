c7643bd5a4137649a608e76ddc4872155d2173d6
Mika Rinne <mika.rinne@oracle.com>
2019-04-10 16:22:00 +0300

    testing json

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/uploadrecording', function(req, res) {
             { 
                 //console.log(req.body.sensorData);
                 var data = getValues(JSON.parse(req.body.sensorData), "urn:mrn:signalk:uuid:3a528d02-e2a1-4e1a-86b9-4de94433543f");
-                for(int j=0; j < data.length; j++)
+                for(var j=0; j < data.length; j++)
                 {
                     console.log(data[j]);
                     var s = JSON.stringify(data[j]);
