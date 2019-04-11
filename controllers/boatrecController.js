a7c6058353b0a5b5a526fdcac589f628c0cb97d1
Mika Rinne <mika.rinne@oracle.com>
2019-04-11 14:29:40 +0300

    testing

--- controllers/boatrecController.js
+++ controllers/boatrecController.js
@@ -9,10 +9,12 @@ app.controller('boatrecController', function($location, $http, $rootScope, $scop
             for(var i = 0; i < response.length; i++) {
                 console.log(response[i]);
                 var row = {};
+                /*
                 row.navigation = response[i].['urn:mrn:signalk:uuid:3a528d02-e2a1-4e1a-86b9-4de94433543f'].navigation;
                 row.performance = response[i].['urn:mrn:signalk:uuid:3a528d02-e2a1-4e1a-86b9-4de94433543f'].performance;
                 row.environment = response[i].['urn:mrn:signalk:uuid:3a528d02-e2a1-4e1a-86b9-4de94433543f'].environment;
                 data.push(row);
+                */
             }
             $scope.boatrecData = data;
         });
