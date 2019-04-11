98f63de2dd09f00f19aa662b018f3228a8c37194
Mika Rinne <mika.rinne@oracle.com>
2019-04-11 18:31:08 +0300

    ui

--- controllers/boatrecController.js
+++ controllers/boatrecController.js
@@ -1,13 +1,14 @@
 app.controller('boatrecController', function($location, $http, $rootScope, $scope, $routeParams, $interval, $timeout)
 {
 	$scope.dbtestuser = null;
+    $scope.data.uuid = 'urn:mrn:signalk:uuid:3a528d02-e2a1-4e1a-86b9-4de94433543f';
 
 	if($location.path() == '/')
 	{
 		var data = [];
         $http.get('/data').success(function(response, err) {
             var s = JSON.stringify(response);
-            var s2 = replaceAll(s, 'urn:mrn:signalk:uuid:3a528d02-e2a1-4e1a-86b9-4de94433543f', 'uuid');
+            var s2 = replaceAll(s, $scope.data.uuid, 'uuid');
             response = JSON.parse(s2);
             for(var i = 0; i < response.length; i++) {
                 console.log(response[i]);
@@ -25,6 +26,26 @@ app.controller('boatrecController', function($location, $http, $rootScope, $scop
         $http.get('/testconnection').success(function(response, err) {
             console.log(response);
             $scope.dbtestuser = response.user;
+        });
+    }
+    
+    $scope.setUuid = function(uuid) {
+        $rootScope.data.uuid = uuid;
+        console.log($rootScope.data.uuid);
+        var data = [];
+        $http.get('/data').success(function(response, err) {
+            var s = JSON.stringify(response);
+            var s2 = replaceAll(s, $scope.data.uuid, 'uuid');
+            response = JSON.parse(s2);
+            for(var i = 0; i < response.length; i++) {
+                console.log(response[i]);
+                var row = {};
+                row.navigation  = response[i].uuid.navigation;
+                row.performance = response[i].uuid.performance;
+                row.environment = response[i].uuid.environment;
+                data.push(row);
+            }
+            $scope.boatrecData = data;
         });
     }
     
