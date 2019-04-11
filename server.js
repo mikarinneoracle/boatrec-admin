76b2d38b89b1777e8864f6f0edb349e8f7dd3b44
Mika Rinne <mika.rinne@oracle.com>
2019-04-11 15:26:08 +0300

    testing

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.post('/uploadrecording', function(req, res) {
                     if(err)
                         break;
                     var s = JSON.stringify(data[i]);
+                    count++;
                     console.log("Inserting row " + count);
                     connection.execute(
                         'INSERT INTO j_boatrec (recording) VALUES (:bv)',
@@ -65,8 +66,6 @@ app.post('/uploadrecording', function(req, res) {
                         { autoCommit: true }, function(err) {
                             if (err) {
                                 error = err;
-                            } else {
-                                count++;
                             }
                     });
                 }
