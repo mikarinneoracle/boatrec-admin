8ae034d258b9b57326dcdff4a0a2c1468606a377
Mika Rinne <mika.rinne@oracle.com>
2019-04-12 17:56:29 +0300

    fixes

--- server.js
+++ server.js
@@ -338,3 +338,12 @@ app.get('/createdb', function(req, res) {
     }
   });
 });
+
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function escapeRegExp(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
